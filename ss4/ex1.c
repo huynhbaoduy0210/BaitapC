58395d05b925a54a2268c6f2516f4b3a5eb58fc7
huynhbaoduy0210 <149989841+huynhbaoduy0210@users.noreply.github.com>
2023-12-08 08:26:07 +0700

    change code

---
+++ ss4/ex1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int i;
+    printf("nhap so nguyen");
+    scanf_s("%d",&i);
+    if (i%3==0 && i%5==0){
+        printf("chia het");
+        scanf("%d",&i);
+    }else {
+        printf("khong chia het");
+        scanf("%d",&i);
+    }
+    return 0;

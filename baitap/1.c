7057ca2466bc33fbc06dd7c29bbbeb2e43d740dc
huynhbaoduy0210 <149989841+huynhbaoduy0210@users.noreply.github.com>
2023-12-26 23:43:18 +0700

    change code

---
+++ baitap/1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+struct City {
+    char name[50];
+    int income;
+    int population;
+    float literacyRate;
+};
+
+int main() {
+    struct City cities[3];
+    int i, maxIncomeIndex = 0, maxLiteracyIndex = 0;
+
+
+    for (i = 0; i < 3; i++) {
+        printf(" nhap thong tin thanh pho %d:\n", i + 1);
+        printf("Ten thanh pho: ");
+        scanf("%s", cities[i].name);
+        printf("Tong thu nhap: ");
+        scanf("%d", &cities[i].income);
+        printf("dan so: ");
+        scanf("%d", &cities[i].population);
+        printf("ti le biet doc viet: ");
+        scanf("%f", &cities[i].literacyRate);
+    }
+
+
+    for (i = 1; i < 3; i++) {
+        if (cities[i].income > cities[maxIncomeIndex].income) {
+            maxIncomeIndex = i;
+        }
+    }
+
+
+    for (i = 1; i < 3; i++) {
+        if (cities[i].literacyRate > cities[maxLiteracyIndex].literacyRate) {
+            maxLiteracyIndex = i;
+        }
+    }
+
+
+    printf("\nthanh pho co tong thu nhap cao nhat la %s voi tong thu nhap la %d.\n", cities[maxIncomeIndex].name, cities[maxIncomeIndex].income);
+    printf("thanh pho co ti le biet doc viet cao nhat la %s voi ti le biet đoc viet la %.2f.\n", cities[maxLiteracyIndex].name, cities[maxLiteracyIndex].literacyRate);
+
+    return 0;
+}
---
+++ tren lop/struct.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+#define PERSON_NUM 5
+
+typedef struct {
+    char name[20];
+    char sex;
+    int age;
+    double height;
+    double weight;
+}person_t;
+ int main (void){
+double height_sum, weight_sum, height_ave, weight_ave;
+height_sum = weight_sum
+
+ }
+
+
+
